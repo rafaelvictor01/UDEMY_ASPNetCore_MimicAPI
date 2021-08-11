3a034e3d31a1c3832b536649d6c6fa368d59a504
Rafael Victor <rafaelvictor.bernardes@gmail.com>
2021-08-10 21:43:20 -0300

    Criação das rotas e utlização dos verbos Http

--- MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/Controllers/PalavrasController.cs
@@ -3,6 +3,7 @@ using MimicAPI.Database;
 
 namespace MimicAPI.Controllers
 {
+    [Route("palavras")]
     public class PalavrasController : ControllerBase
     {
         private readonly MimicContext _banco;
@@ -10,28 +11,39 @@ namespace MimicAPI.Controllers
         {
             _banco = banco;
         }
+
+        [HttpGet]
+        [Route("")]
         public IActionResult ObterTodas()
         {
             return new JsonResult(_banco.palavras);
         }
 
+        [HttpGet]
+        [Route("{Id}")]
         public ActionResult Obter(int Id)
         {
             return new JsonResult(_banco.palavras.Find(Id));
         }
 
+        [HttpPost]
+        [Route("")]
         public ActionResult Cadastrar(Models.Palavra palavra)
         {
             _banco.palavras.Add(palavra);
             return Ok();
         }
 
-        public ActionResult Atualizar(int Id, Models.Palavra palavra)
+        [HttpPut]
+        [Route("")]
+        public ActionResult Atualizar(Models.Palavra palavra)
         {
             _banco.palavras.Update(palavra);
             return Ok();
         }
 
+        [HttpDelete]
+        [Route("{Id}")]
         public ActionResult Remover(int Id)
         {
             _banco.palavras.Remove(_banco.palavras.Find(Id));
